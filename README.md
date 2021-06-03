6cc1b2516408a0d5fa70b85be540d2da37a654ec
Omar Moreno <c.j_omar@hotmail.com>
2021-06-03 17:04:35 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Permission Laravel
+Shell to assign permissions to Laravel project
+
+## How to use
+1. Download script with download zip or with `git clone`
+2. Enter to directory where you download the script
+3. Execute the script with `sh permissions.sh`
+
+### Aditional Information
+The script generate files with the services enable or disable
---
+++ permissions.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "Escriba el nombre del directorio del proyecto:"
+read directory
+
+ROUTE="/home/projects/$directory/"
+USER=${whoami}
+
+echo $ROUTE
+
+if [ -d "$ROUTE" ]; then
+        $(sudo chown -R $USER:www-data $ROUTE)
+        $(sudo find $ROUTE -type f -exec chmod 664 {} \;)
+        $(sudo find $ROUTE -type d -exec chmod 775 {} \;)
+        $(sudo chgrp -R www-data "${ROUTE}storage" "${ROUTE}bootstrap/cache")
+        $(sudo chmod -R ug+rwx "${ROUTE}storage" "${ROUTE}bootstrap/cache")
+        echo "Se terminaron de asignar permisos"
+else
+        echo "No existe el directorio a asignar permisos"
+fi
